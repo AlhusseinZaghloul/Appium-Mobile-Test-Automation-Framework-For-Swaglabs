f7824e7a769497ef669a985edb64217b6e7823b7
Alhussein Zaghloul <152105415+AlhusseinZaghloul@users.noreply.github.com>
2025-03-27 14:23:50 +0200

    Add Allure step annotations to ElementActions methods for enhanced reporting

--- src/main/java/utils/ElementsActions.java
+++ src/main/java/utils/ElementsActions.java
@@ -1,29 +1,31 @@
 package utils;
 
 import io.appium.java_client.android.AndroidDriver;
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 
 public class ElementsActions {
 
 
     //code to perform sendKeys operation
+    @Step("Sending data: {data} to element: {locator}")
     public static void sendData(AndroidDriver driver , By locator, String data) {
-
         Waits.waitForElementVisible(driver, locator);
         driver.findElement(locator).sendKeys(data);
         LogsUtils.info("Data entered: ", data , " in element: ", locator.toString());
     }
 
+    @Step("Clicking on element: {locator}")
     //code to perform click operation
     public static void clicking(AndroidDriver driver , By locator) {
-
         Waits.waitForElementClickable(driver, locator);
         driver.findElement(locator).click();
         LogsUtils.info("Clicked on element: ", locator.toString());
     }
+
+    @Step("Getting attribute from element: {locator}")
     //get attribute from element
     public static String getAttributeFromElement(AndroidDriver driver , By locator , String attribute) {
-
         Waits.waitForElementVisible(driver, locator);
         LogsUtils.info("Getting attribute: ", attribute , " from element: ", locator.toString());
         return driver.findElement(locator).getDomAttribute(attribute);
