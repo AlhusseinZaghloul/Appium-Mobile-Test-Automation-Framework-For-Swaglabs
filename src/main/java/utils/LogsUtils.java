eb73be838a5ee2b2d7be0b43914198a40e265c69
Alhussein Zaghloul <152105415+AlhusseinZaghloul@users.noreply.github.com>
2025-04-02 01:06:55 +0200

    Enhance LogsUtils with detailed documentation and improve logger retrieval method

--- src/main/java/utils/LogsUtils.java
+++ src/main/java/utils/LogsUtils.java
@@ -3,16 +3,27 @@ package utils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * Utility class for providing logging functionalities using Log4j 2.
+ * This class provides static methods to easily log messages at different severity levels.
+ */
 public class LogsUtils {
+    //Private constructor to prevent instantiation of this utility class.
     private LogsUtils() {}
 
     public static final String LOGS_PATH = "test-outputs/Logs";
 
+    /**
+     * Retrieves the Logger instance for the class that calls this method.
+     * This approach ensures that log messages are associated with the originating class,
+     * making it easier to trace the source of the logs.
+     *
+     * @return The Logger instance for the calling class.
+     */
     private static Logger logger() {
+        // Get the logger for the class that calls this method
         return LogManager.getLogger(Thread.currentThread().getStackTrace()[3].getClassName());
     }
-
-    // LogUtils.info("username is: ", data.username); []
 
     public static void trace(String... message) {
         logger().trace(String.join(" ", message));
